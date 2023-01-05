1730eff8878b065c370e96b49506925af93a0001
slashack <funkcharles@hotmail.com>
2023-01-05 12:30:47 -0500

    Add proper grid resizing logic and prompt

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 //Grid creation
 let squareContainer = document.getElementById("square-container");
 //container for individual squares instantiated
-let squareArray = {};
+let squareArray = [];
 //Creates individual square in array, assigns class, adds mouseover listener, and adds to DOM
-function createSquare(i) { 
+function createSquare(gridDimensions) { 
+    //Remove all prior squares if pertinent
+    while (squareContainer.firstChild) {
+        squareContainer.firstChild.remove();
+    }
+    //Loop for element creation
+    for (let i = 0; i < Math.pow(gridDimensions, 2); i++) {
     //element creation
     squareArray[i] = document.createElement("div");
     //adds square class
     squareArray[i].classList.add("grid-square")
     //Adds event listener to all elements, mouseover activated class is added
     squareArray[i].addEventListener("mouseover", () => squareArray[i].classList.add("activated-grid-square"));
+    //Add height & width style to each individual element
+    let squareDimensions = 400 / gridDimensions;
+    squareArray[i].style.height = `${squareDimensions}px`;
+    squareArray[i].style.width = `${squareDimensions}px`;
     //add element to DOM
     squareContainer.appendChild(squareArray[i]);
+    } 
 }
-//Index and function call for all 256 square creation
-let gridDimensions = 256;
-for (let i = 0; i < gridDimensions; i++) {
-    createSquare(i);
-}
+//Default dimensions and grid creation
+let gridDimensions = 16;
+
+//User specified dimensions and grid creation  
+gridDimensions = prompt("Enter grid size: ")
+createSquare(gridDimensions) 
+    
