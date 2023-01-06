21af6503e705e6d443012d58903276dc104c42ae
slashack <funkcharles@hotmail.com>
2023-01-05 20:55:52 -0500

    Add lock grid feature

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 //Grid creation
+let notLocked = true;
 let squareContainer = document.getElementById("square-container");
 //container for individual squares instantiated
 let squareArray = [];
 //Creates individual square in array, assigns class, adds mouseover listener, and adds to DOM
 function createSquare(gridDimensions) { 
-    //Remove all prior squares if pertinent
-    while (squareContainer.firstChild) {
-        squareContainer.firstChild.remove();
+    if (notLocked === true) {
+        //Remove all prior squares if pertinent
+        while (squareContainer.firstChild) {
+            squareContainer.firstChild.remove()
+        }
+        //Loop for element creation
+        for (let i = 0; i < Math.pow(gridDimensions, 2); i++) {
+            //element creation
+            squareArray[i] = document.createElement("div");
+            //adds square class
+            squareArray[i].classList.add("grid-square")
+            //Adds event listener to all elements, mouseover activated class is added
+            squareArray[i].addEventListener("mouseover", activateSquare);
+            function activateSquare() {
+                squareArray[i].classList.add("activated-grid-square");
+            }
+            //Add height & width style to each individual element
+            let squareDimensions = 400 / gridDimensions;
+            squareArray[i].style.height = `${squareDimensions}px`;
+            squareArray[i].style.width = `${squareDimensions}px`;
+            //add element to DOM
+            squareContainer.appendChild(squareArray[i]);
+        } 
     }
-    //Loop for element creation
-    for (let i = 0; i < Math.pow(gridDimensions, 2); i++) {
-    //element creation
-    squareArray[i] = document.createElement("div");
-    //adds square class
-    squareArray[i].classList.add("grid-square")
-    //Adds event listener to all elements, mouseover activated class is added
-    squareArray[i].addEventListener("mouseover", () => squareArray[i].classList.add("activated-grid-square"));
-    //Add height & width style to each individual element
-    let squareDimensions = 400 / gridDimensions;
-    squareArray[i].style.height = `${squareDimensions}px`;
-    squareArray[i].style.width = `${squareDimensions}px`;
-    //add element to DOM
-    squareContainer.appendChild(squareArray[i]);
-    } 
-}
+    let activateSquare = activateSquare;
+}   
+
 //Default dimensions and grid creation
 let gridDimensions = 16;
 createSquare(gridDimensions);
 //User specified dimensions and grid creation  
-let button = document.querySelector('button');
-button.addEventListener("click", () => {
-gridDimensions = prompt("Enter grid size, max 100: ")
-    if (gridDimensions <= 100) {
-        createSquare(gridDimensions);
-    }
+let slider = document.getElementById("slider");
+slider.oninput = () => {
+    gridDimensions = slider.value; 
+    createSquare(gridDimensions);
+    
+}
+
+//Query buttons & Reset Grid button
+let buttons = document.querySelectorAll('button');
+
+buttons[0].addEventListener('click', () => {
+    createSquare(gridDimensions);
 });
+//Save Drawing Button
+
+function toggle() {
+    notLocked = !notLocked;
+    if(!notLocked) {
+        // for (let i = 0; i < Math.pow(gridDimensions, 2); i++){
+        //     squareArray[i].removeEventListener("mouseover", () => squareArray[i].classList.add("activated-grid-square"));
+        // }
+        buttons[1].innerText = "Unlock Drawing";
+    } else {
+        // for (let i = 0; i < Math.pow(gridDimensions, 2); i++){
+        //    // squareArray[i].classList.add("activated-grid-square"));
+        // }
+        buttons[1].innerText = "Lock Drawing";    
+    }
+};
+
     
